990b29209748c6e079532c552904befd226176b7
Olciaaa <57765820+Olciaaa@users.noreply.github.com>
2024-10-30 23:46:15 +0100

    HMSS-179 - Retrofit integration (#31)
    * retrofit integration
    
    * retrofit integration

--- HealthMonitoringSystem-shared/src/main/java/agh/edu/pl/healthmonitoringsystem/client/PatientApi.java
+++ HealthMonitoringSystem-shared/src/main/java/agh/edu/pl/healthmonitoringsystem/client/PatientApi.java
@@ -4,10 +4,9 @@ import retrofit2.Call;
 import retrofit2.http.GET;
 import agh.edu.pl.healthmonitoringsystem.response.Result;
 import retrofit2.http.Path;
-
 import java.util.List;
 
 public interface PatientApi {
-    @GET("/{patientId}/results")
+    @GET("/api/patients/{patientId}/results")
     Call<List<Result>> getPatientResults(@Path("patientId") Long patientId);
 }
