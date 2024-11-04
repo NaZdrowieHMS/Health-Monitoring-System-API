0dd7caf57f5749a931eaa4879380d164340ebfef
kkkucia <11.karolinakucia@gmail.com>
2024-11-04 21:02:47 +0100

    HMSS-195 Spring retry. Database secure

--- HealthMonitoringSystemApplication/src/main/java/agh/edu/pl/healthmonitoringsystem/HealthMonitoringSystemApplication.java
+++ HealthMonitoringSystemApplication/src/main/java/agh/edu/pl/healthmonitoringsystem/HealthMonitoringSystemApplication.java
@@ -2,8 +2,12 @@ package agh.edu.pl.healthmonitoringsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy
+@EnableRetry
 public class HealthMonitoringSystemApplication {
 
 	public static void main(String[] args) {
